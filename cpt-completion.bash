479c4d1b091bc0ab6b45612ef3acde7fb9407749
Hiro Kojima <guitarsaiko@gmail.com>
2020-02-28 11:14:32 +0900

    Change directory structure to mange templates

--- cpt-completion.bash
+++ cpt-completion.bash
@@ -3,7 +3,7 @@
 _dothis_completions()
 {
 
-  directoryList=($(ls -d $CPT_ROOT/*/ | sed -e  "s|$CPT_ROOT||" | tr -d / | grep -v bin ));
+  directoryList=($(ls -d $CPT_ROOT/templates/*/ | sed -e  "s|${CPT_ROOT}/templates||" | tr -d / | grep -v bin ));
   COMPREPLY=($(compgen -W "${directoryList[*]}" "${COMP_WORDS[1]}"))
 
 }
